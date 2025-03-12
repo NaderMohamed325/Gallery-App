97dc422fe4c713d311cffcfd9f8e16e114d80129
NaderMohamed325 <nadoorclasher@gmail.com>
2025-03-12 23:17:27 +0200

    Add POST route for login in viewRouter with Login controller

--- src/routers/viewRouter.ts
+++ src/routers/viewRouter.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { loginViewer, registerViewer, registerUser } from '../controllers/viewController';
+import { loginViewer, registerViewer, registerUser, Login } from '../controllers/viewController';
 
 const viewRouter = express.Router();
 
 viewRouter.get('/api/v1/login', loginViewer);
+viewRouter.post('/api/v1/login', Login);
 viewRouter.get('/api/v1/register', registerViewer);
 viewRouter.post('/api/v1/register', registerUser);
 
