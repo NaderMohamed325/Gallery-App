86afe81e61976831647483e2bc0b3f3e42295440
NaderMohamed325 <nadoorclasher@gmail.com>
2025-03-12 16:11:02 +0200

    Add MongoDB connection and favicon handling to server setup

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,26 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import { rateLimit } from 'express-rate-limit';
 import dotenv from 'dotenv';
+dotenv.config();
 import { viewRouter } from './routers/viewRouter';
 import { ErrorHandler } from './utils/errorHandler';
 import AppError from './utils/AppError';
-
+import mongoose from 'mongoose';
+mongoose.connect('mongodb://localhost:27017/Cloud').then(() => {
+  console.log(`DB Connected`);
+});
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-  standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-  // store: ... , // Redis, Memcached, etc. See below.
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  standardHeaders: 'draft-8',
+  legacyHeaders: false,
 });
 
 const app = express();
+app.get('/favicon.ico', (req: Request, res: Response) => {
+   res.status(204).end();
+});
+
 app.use(helmet());
 app.use(express.json());
 app.use(ErrorHandler);
@@ -28,12 +35,11 @@ dotenv.config();
 const port = parseInt(process.env.PORT || '3000', 10);
 
 app.use(viewRouter);
-// Start server
 
 app.use(ErrorHandler);
 app.all('*', (req: Request, _res: Response, next: NextFunction) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`, `${__dirname}/views`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
