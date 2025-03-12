23c38c91a436a26ff13875188a0b2e59de203d2b
NaderMohamed325 <nadoorclasher@gmail.com>
2025-03-12 23:17:08 +0200

    Add session management with MongoDB store and integrate dashboard router

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,20 @@ import { rateLimit } from 'express-rate-limit';
 import dotenv from 'dotenv';
 dotenv.config();
 import { viewRouter } from './routers/viewRouter';
+import { dashboardRouter } from './routers/dashboardRouter';
 import { ErrorHandler } from './utils/errorHandler';
 import AppError from './utils/AppError';
 import mongoose from 'mongoose';
+import session from 'express-session';
+import { default as connectMongoDBSession } from 'connect-mongodb-session';
+
+const monstore = connectMongoDBSession(session);
 mongoose.connect('mongodb://localhost:27017/Cloud').then(() => {
   console.log(`DB Connected`);
+});
+var store = new monstore({
+  uri: 'mongodb://localhost:27017/Cloud',
+  collection: 'sessions',
 });
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -20,10 +29,18 @@ const limiter = rateLimit({
 
 const app = express();
 app.get('/favicon.ico', (req: Request, res: Response) => {
-   res.status(204).end();
+  res.status(204).end();
 });
 
 app.use(helmet());
+app.use(
+  session({
+    secret: process.env.SESSION_KEY || 'MY SECRET KEY',
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  }),
+);
 app.use(express.json());
 app.use(ErrorHandler);
 app.set('view engine', 'ejs');
@@ -35,6 +52,7 @@ dotenv.config();
 const port = parseInt(process.env.PORT || '3000', 10);
 
 app.use(viewRouter);
+app.use(dashboardRouter);
 
 app.use(ErrorHandler);
 app.all('*', (req: Request, _res: Response, next: NextFunction) => {
