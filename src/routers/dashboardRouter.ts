40e7c44febb08e530b9d071b0545dac4bb401314
NaderMohamed325 <nadoorclasher@gmail.com>
2025-03-13 13:53:49 +0200

    Add upload route to dashboard router with authentication

--- src/routers/dashboardRouter.ts
+++ src/routers/dashboardRouter.ts
@@ -1,10 +1,13 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { dashboardViewer, dashboardLogout } from '../controllers/dashboardController';
 import { isAuthenticated } from '../utils/middlewares/isAuthenticated';
+
+import { catchAsync } from '../utils/catchAsync';
 
 const dashboardRouter = express.Router();
 
 dashboardRouter.get('/api/v1/dashboard', isAuthenticated, dashboardViewer);
 dashboardRouter.post('/api/v1/logout', isAuthenticated, dashboardLogout);
+dashboardRouter.post('/api/v1/upload', isAuthenticated);
 
 export { dashboardRouter };
