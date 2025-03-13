f1844d9a5e35f301b7f67e871d445a1c2aa03998
NaderMohamed325 <nadoorclasher@gmail.com>
2025-03-13 13:53:46 +0200

    Integrate Cloudinary for image retrieval in dashboard viewer

--- src/controllers/dashboardController.ts
+++ src/controllers/dashboardController.ts
@@ -1,7 +1,25 @@
 import { Request, Response, NextFunction } from 'express';
+import { v2 as cloudinary } from 'cloudinary';
 
-const dashboardViewer = (req: Request, res: Response) => {
-  res.render('dashboard');
+// Configuration
+cloudinary.config({
+  cloud_name: 'dfnyw08sg',
+  api_key: '681699289446763',
+  api_secret: process.env.API_SECRET || 'GEfPlSqjCPIAeaoVXcmyS1-0rQc',
+});
+
+
+
+const dashboardViewer = async (req: Request, res: Response) => {
+  const { resources } = await cloudinary.search
+    .expression('folder:uploads')
+    .sort_by('public_id', 'desc')
+    .max_results(30)
+    .execute();
+  const images = resources.map((file: { secure_url: any }) => ({
+    url: file.secure_url,
+  }));
+  res.render('dashboard', { images });
 };
 
 const dashboardLogout = (req: Request, res: Response, next: NextFunction) => {
@@ -12,5 +30,7 @@ const dashboardLogout = (req: Request, res: Response, next: NextFunction) => {
     res.redirect('/api/v1/login');
   });
 };
+
+
 
 export { dashboardViewer, dashboardLogout };
