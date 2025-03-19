322945dd27fff186fb131de307fb7ec70724c6c7
NaderMohamed325 <nadoorclasher@gmail.com>
2025-03-19 10:32:03 +0200

    Add upload functionality to dashboard router with photo upload handler , i have issue with photo display

--- src/routers/dashboardRouter.ts
+++ src/routers/dashboardRouter.ts
@@ -1,13 +1,12 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import { dashboardViewer, dashboardLogout } from '../controllers/dashboardController';
 import { isAuthenticated } from '../utils/middlewares/isAuthenticated';
-
-import { catchAsync } from '../utils/catchAsync';
+import { uploadPhoto } from '../controllers/uploadController';
 
 const dashboardRouter = express.Router();
 
 dashboardRouter.get('/api/v1/dashboard', isAuthenticated, dashboardViewer);
 dashboardRouter.post('/api/v1/logout', isAuthenticated, dashboardLogout);
-dashboardRouter.post('/api/v1/upload', isAuthenticated);
+dashboardRouter.post('/api/v1/upload', isAuthenticated, uploadPhoto);
 
 export { dashboardRouter };
